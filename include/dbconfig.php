7b955bd6b349e4c15894a0956a78a4555c40f3be
thapelo afrika <thapelo.afrika@hellogroup.co.za>
2014-10-09 12:30:16 +0200

    Working on audits

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -10,5 +10,5 @@
  define('DB_HOST','localhost');
  define('DB_USER','root');
  define('DB_PASS','');
- define('DB_NAME','test');
+ define('DB_NAME','transx');
  define('DB_PORT','3306');
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once './include/functions.php';
-require_once './include/db_access.php';
+require_once '/include/functions.php';
+require_once '/include/db_access.php';
 
 $dbObject = new Databases();
 
@@ -10,15 +10,14 @@ switch($action) { //Switch case for value of action
   case "login": 
     login();       
   break;
-  case "getBalance": 
-    getBalance();       
+  case "get_balance": 
+    get_balance();       
   break;
 }
  
 function login()
 {                  
-    $resultset = $GLOBALS['dbObject']->runQuery("SELECT * FROM t_customers WHERE login_str = '".mysqli_real_escape_string($GLOBALS['dbObject']->conn,$_POST['login_str'])."' AND password = '".mysqli_real_escape_string($GLOBALS['dbObject']->conn,$_POST['password'])."' LIMIT 1;");
-    $row = $resultset->fetch_array(MYSQLI_ASSOC);
+    $resultset = $GLOBALS['dbObject']->runQuery("SELECT * FROM t_customers WHERE login_str = '".mysqli_real_escape_string($GLOBALS['dbObject']->conn,$_POST['login_str'])."' AND password = '".mysqli_real_escape_string($GLOBALS['dbObject']->conn,$_POST['password'])."' LIMIT 1;");    
     
     if(mysqli_num_rows($resultset) === 0)
     {
@@ -29,6 +28,8 @@ function login()
         echo $jsonResponse;
         exit;
     }else{
+        $row = $resultset->fetch_array(MYSQLI_ASSOC);
+        
         //Generare session id
         $random = substr(number_format(time() * mt_rand(),0,'',''),0,10);
         $str = $row['cust_id'].$row['company_name'].$row['contact_num'].$random;
@@ -53,14 +54,64 @@ function login()
   //echo json_encode($return);
 }
 
-function getBalance()
-{
-    if(!checksessionid($_POST['sessionid']))
+function get_balance()
+{    
+    if(!checksessionid($_POST['session_id']))
     {
         $jsonResponse = array('status' => 'Fail', 'error' => 'Invalid request');
         $jsonResponse = json_encode($jsonResponse);
         
         echo $jsonResponse;
         exit;
+    }    
+    
+    //{"action":"get_balance","session_id":$('#sessionid').val(),"userid":$('#userid').val()}
+    //Get balances for this user
+    $resultset = $GLOBALS['dbObject']->runQuery("SELECT * FROM t_wallet WHERE uid = '".mysqli_real_escape_string($GLOBALS['dbObject']->conn,$_POST['uid'])."' LIMIT 1;");            
+    $row = $resultset->fetch_array(MYSQLI_ASSOC); 
+    
+    if(mysqli_num_rows($resultset) === 0)
+    {        
+        $jsonResponse = array('status' => 'Fail', 'error' => 'Could not find this userid:'.$_POST['uid'].'');
+        $jsonResponse = json_encode($jsonResponse);
+        
+        echo $jsonResponse;
+        exit;
+    }else{                               
+        log_wallet_audit('wa0004',$_POST['uid'],'','','','');
+                
+        $jsonResponse = array('status' => 'Success', 'error' => '','pool_balance' => $row['pool_balance'],'kcm_wallet_balance' => $row['kcm_wallet'], 'glo_wallet_balance' => $row['glo_wallet']);                                                  
+        $jsonResponse = json_encode($jsonResponse);
+        
+        echo $jsonResponse;
+        exit;
+    }
+}
+
+
+//Check sessionid validity
+function checksessionid($sessionid)
+{
+    $resultset = $GLOBALS['dbObject']->runQuery("SELECT count(*) FROM t_customerslogin WHERE session_id = '".mysqli_real_escape_string($GLOBALS['dbObject']->conn,$sessionid)."' AND (DATEDIFF(`expire_date`,now())) > 0 LIMIT 1;");
+    
+    if(mysqli_num_rows($resultset) === 0)
+    {
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function log_wallet_audit($action,$uid,$value,$sourcepool,$destinationpool,$voucherserial)
+{
+    if (!$GLOBALS['dbObject']->runQuery("INSERT INTO t_walletaudit (uid,action,value,source_pool,destination_pool,voucher_serial_number) VALUES ('".$uid."','".$action.",'".$value.",'".$sourcepool.",'".$destinationpool.",'".$voucherserial.";")) 
+    {                   
+        writelog("log.log", "[AUDIT LOG] Failed to write to audit log");
+        exit;
+    }else{
+        $resultset = $GLOBALS['dbObject']->runQuery("SELECT LAST_INSERT_ID() as id FROM t_walletaudit;");            
+        $row = $resultset->fetch_array(MYSQLI_ASSOC); 
+        
+        $GLOBALS['dbObject']->runQuery("UPDATE t_walletaudit SET w_reference='".str_pad($row['id'], 6, '0', STR_PAD_LEFT)."' WHERE Id='".$row['id']."';");    
     }
 }
