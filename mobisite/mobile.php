a0eb9c1e98c9765d77e1dc86277259fe38712ee8
thapelo <thapeloa@gmail.com>
2014-10-11 22:20:26 +0200

    Work done 11/10/2014 Added mobisite folder and generatevouchers

---
+++ mobisite/mobile.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Login | KCMOBILE</title>
+        <link rel='stylesheet' type='text/css' href='style/style.css'>
+        <meta name='robots' content='noindex,follow' />
+    </head>
+    <body>
+        <div id='mobile-container'>
+            <div id='mobile-header'></div>
+            <div id='mobile-body'>
+                <iframe id='mobile-iframe' src='loginpage.php' ></iframe>
+            </div>
+            <div id='mobile-footer'></div>
+        </div>
+    </body>
+</html>
